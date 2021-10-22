5c2cd4e0d9a10736302f9f9c620f192412830eec
tomaspasto24 <tomaspasto24@gmail.com>
2021-10-22 20:25:09 -0300

    Ubicación Provisoria (string)

---
+++ src/Library/Ubicacion.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bot
+{
+    public class Ubicacion
+    {
+        //En un futuro usar Geolocalización.
+        private string zona;
+        private string departamento;
+        private string direccion;
+
+        public Ubicacion(string direccion, string zona, string departamento)
+        {
+            this.direccion = direccion;
+            this.zona = zona;
+            this.departamento = departamento;
+        }
+    }
