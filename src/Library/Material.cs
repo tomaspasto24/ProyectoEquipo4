9fe62ee88ce5ec38f5d0a4331c4b86024073d4dc
tomaspasto24 <tomaspasto24@gmail.com>
2021-10-28 19:09:44 -0300

    Clase Material Comentada

--- src/Library/Material.cs
+++ src/Library/Material.cs
@@ -6,30 +6,46 @@ namespace Bot
 {
     public class Material
     {
-        private string nombre;
-        private int cantidad;
-        private int precio;
-        private List<string> palabrasClave = new List<string>();
-        private List<string> listaHabilitaciones = new List<string>();
+        private string name;
+        private int quantity;
+        private int price;
+        private List<string> keyWords = new List<string>(); // Palabras clave
+        private List<string> listRatings = new List<string>(); // Lista Habilitaciones
 
-        public string Nombre
+        /// <summary>
+        /// Devuelve Atributo nombre del la clase Material.
+        /// </summary>
+        /// <value>String</value>
+        public string Name
         {
             get
             {
-                return this.nombre;
+                return this.name;
             }
         }
 
+        /// <summary>
+        /// Constructor de la clase Material que se encarga de asignar valores a los 
+        /// atributos name, quantity y price. Construye el Material. En caso de no tener 
+        /// precio, se asigna 0 a su atributo.
+        /// </summary>
+        /// <param name="nombre">String nombre.</param>
+        /// <param name="cantidad">Int cantidad</param>
+        /// <param name="precio">Int precio</param>
         public Material(string nombre, int cantidad, int precio)
         {
-            this.nombre = nombre;
-            this.cantidad = cantidad;
-            this.precio = precio;   
+            this.name = nombre;
+            this.quantity = cantidad;
+            this.price = precio;   
         }
 
-        public void AgregarPalabraClave(string palabraClave)
+        /// <summary>
+        /// Agrega una palabra clave a la lista de palabras clave
+        /// </summary>
+        /// <param name="palabraClave"></param>
+        public void AddKeyWord(string palabraClave) // Agregar palabras clave
         {
-            this.palabrasClave.Add(palabraClave);
+            this.keyWords.Add(palabraClave);
         }
 
         /// <summary>
@@ -39,39 +55,56 @@ namespace Bot
         /// <param name="indicePalabraClave">Indice de la palabra clave que se quiera eliminar.
         /// Se obtiene con la función DevolverPalabrasClave.</param>
         /// <returns></returns>
-        public bool EliminarPalabraClave(int indicePalabraClave)
+        public bool DeleteKeyWord(int indicePalabraClave)
         {
-            return palabrasClave.Remove(palabrasClave[indicePalabraClave]);
+            return keyWords.Remove(keyWords[indicePalabraClave]);
         }
 
-        public string DevolverPalabrasClave()
+        /// <summary>
+        /// Método que devuelve todas las Palabras Clave que contiene el Material.
+        /// </summary>
+        /// <returns>String</returns>
+        public string ReturnKeyWords() 
         {
             StringBuilder resultado = new StringBuilder("Palabras Clave: \n");
             int contador = 0;
 
-            foreach(string palabra in this.palabrasClave)
+            foreach(string palabra in this.keyWords)
             {
                 resultado.Append($"{++contador}- {palabra} \n");
             }
             return resultado.ToString();
         }
 
-        public void AgregarHabilitacion(string habilitacion)
+        /// <summary>
+        /// Agrega una habilitación a la lista de Habilitaciones de la clase Material.
+        /// </summary>
+        /// <param name="habilitacion">String</param>
+        public void AddRating(string habilitacion)
         {
-            listaHabilitaciones.Add(habilitacion);
+            listRatings.Add(habilitacion);
         }
 
-        public bool EliminarHabilitacion(int indiceHabilitacion)
+        /// <summary>
+        /// Elimina una habilitación de la lista de Habilitaciones de la clase Material.
+        /// </summary>
+        /// <param name="indiceHabilitacion">Índice de la Habilitación</param>
+        /// <returns><c>True</c> en caso de que se pueda eliminar, <c>False</c> en caso contrario.</returns>
+        public bool DeleteRating(int indiceHabilitacion)
         {
-            return listaHabilitaciones.Remove(palabrasClave[indiceHabilitacion]);
+            return listRatings.Remove(keyWords[indiceHabilitacion]);
         }
 
-        public string DevolverListaHabilitaciones()
+        /// <summary>
+        /// Retorna la lista de Habilitaciones que tiene el material.s
+        /// </summary>
+        /// <returns>String</returns>
+        public string ReturnListRatings()
         {
             StringBuilder resultado = new StringBuilder("Habilitaciones: \n");
             int contador = 0;
 
-            foreach(string palabra in this.listaHabilitaciones)
+            foreach(string palabra in this.listRatings)
             {
                 resultado.Append($"{++contador}- {palabra} \n");
             }
