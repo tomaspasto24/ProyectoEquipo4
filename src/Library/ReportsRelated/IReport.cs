2f8faad7394ae7bce5c2a3545d7ccc0bc55d5539
[Sebastian Fripp] <[sebastian.fripp@correo.ucu.edu.uy]>
2021-11-03 08:19:03 -0300

    rama actualizada

--- src/Library/ReportsRelated/IReport.cs
+++ src/Library/ReportsRelated/IReport.cs
@@ -2,8 +2,15 @@ using System;
 
 namespace Bot
 {
+    /// <summary>
+    /// interfaz con la firma del metodo que devuelve un reporte
+    /// </summary>
     public interface IReport
     {
+        /// <summary>
+        /// firma del metodo GiveReport
+        /// </summary>
+        /// <returns></returns>
         public String GiveReport();
     }
 
---
+++ test/LibraryTests/TokenTest.cs
@@ -0,0 +1,20 @@
+using System;
+using NUnit.Framework;
+using Bot;
+
+namespace BotTests
+{
+    public class TokenTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        [Test]
+        public void GenerateTokenTest()
+        {
+
+        }
+    }
+}
