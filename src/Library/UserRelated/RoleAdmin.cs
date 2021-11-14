faa57aa2a46df21d82281de88a7ca6ef28f82332
Sebastian Fripp <irubyxrubyx@gmail.com>
2021-11-14 13:24:11 -0300

    cambios en role admin

--- src/Library/UserRelated/RoleAdmin.cs
+++ src/Library/UserRelated/RoleAdmin.cs
@@ -5,7 +5,7 @@ namespace Bot
     /// <summary>
     /// Clase RoleAdmin que hereda de la clase Role
     /// </summary>
-    public class RoleAdmin : Role
+    public class RoleAdmin
     {
         /// <summary>
         /// Se inicializa la lista globalQualificationList
@@ -19,7 +19,7 @@ namespace Bot
         /// <param name="name"></param>
         /// <param name="id"></param>
         /// <returns></returns>
-        public RoleAdmin(String name, int id) : base(name, id)
+        public RoleAdmin()
         {
         }
 
@@ -28,50 +28,20 @@ namespace Bot
         /// </summary>
         /// <param name="company"></param>
         /// <returns></returns>
-        public String GenerateToken(Company company)
+        public string GenerateToken(Company company)
         {
-            String newToken = CodeGeneratortoUserCompany(globalQualificationList);
-            SessionRelated.Instance.DiccUserTokens.Add(newToken, company);
-            return newToken;
-
+            int newToken = TokenGenerator.Instance.GenerateToken(company);
+            SessionRelated.Instance.DiccUserTokens.Add(newToken.ToString(), company);
+            return newToken.ToString();
         }
 
-        /// <summary>
-        /// Metodo estatico para generar un string alfanumerico unico para una lista dada.
-        /// </summary>
-        /// <param name="tokens"></param>
-        /// <returns></returns>
-        private static string CodeGeneratortoUserCompany(List<string> tokens)
-        {
-            string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            var Charsarr = new char[8];
-            var random = new Random();
-
-            for (int i = 0; i < Charsarr.Length; i++)
-            {
-                Charsarr[i] = characters[random.Next(characters.Length)];
-            }
-
-            var resultString = new String(Charsarr);
-
-            if (tokens.Contains(resultString))
-            {
-                return CodeGeneratortoUserCompany(tokens);
-            }
-            else
-            {
-                tokens.Add(resultString);
-                return resultString;
-            }
-        }
-
-        /// <summary>
-        /// Metodo para eliminar las habilitaciones a la lista "globalRatingsList" 
-        /// </summary>
-        /// <param name="rating"></param>
-        public void DeleteRating(string rating)
-        {
-            globalQualificationList.Remove(rating);
-        }
+        // /// <summary>
+        // /// Metodo para eliminar las habilitaciones a la lista "globalRatingsList" 
+        // /// </summary>
+        // /// <param name="rating"></param>
+        // public void DeleteRating(string rating)
+        // {
+        //     globalQualificationList.Remove(rating);
+        // }
     }
 }
