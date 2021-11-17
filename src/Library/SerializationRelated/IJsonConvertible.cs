9c14162193e794ef43d3f946550e9f5b6197602b
Tomás Silva <tomas.silva@correo.ucu.edu.uy>
2021-11-17 14:35:53 -0300

    IJsonConvertible

---
+++ src/Library/SerializationRelated/IJsonConvertible.cs
@@ -0,0 +1,14 @@
+namespace Bot
+{
+    /// <summary>
+    /// Interfaz pública que define el tipo de los objetos que pueden ser serializados y deserializados.
+    /// </summary>
+    public interface IJsonConvertible
+    {
+        /// <summary>
+        /// Método que convierte el objeto a formato JSON en cadena de caraceteres.
+        /// </summary>
+        /// <returns>Objeto convertido a JSON como una cadena de caracteres.</returns>
+        string ConvertToJson();
+    }
+}
