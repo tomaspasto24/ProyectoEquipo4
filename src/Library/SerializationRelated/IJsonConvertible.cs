0e7d556664f56dd6d842c610364eddd11db1f9e4
tomaspasto24 <tomaspasto24@gmail.com>
2021-11-22 10:48:29 -0300

    Implementación en interfaz Serializable

--- src/Library/SerializationRelated/IJsonConvertible.cs
+++ src/Library/SerializationRelated/IJsonConvertible.cs
@@ -6,10 +6,9 @@ namespace Bot
     public interface IJsonConvertible
     {
         /// <summary>
-        /// Método que convierte el objeto a formato JSON en cadena de caraceteres.
+        /// Método que convierte el objeto a formato JSON en cadena de caraceteres y lo escribe en el archivo JSON.
         /// </summary>
-        /// <returns>Objeto convertido a JSON como una cadena de caracteres.</returns>
-        string ConvertToJson();
+        void ConvertToJson();
         
         /// <summary>
         /// Método que carga el archivo JSON y lo inicializa.
